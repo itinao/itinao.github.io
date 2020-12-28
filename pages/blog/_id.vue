89f27dac8967fef7fef0d953aebb0f2152ef674a
itinao <itinao@gmail.com>
2020-12-29 01:55:52 +0900

    add page

---
+++ pages/blog/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    {{ $route.params.id }}
+  </div>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
