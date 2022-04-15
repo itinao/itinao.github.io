79b7410bfcc4d50f8ea9a591b3eac68010ae7ad8
itinao <itinao@users.noreply.github.com>
2022-04-15 08:55:14 +0000

    deploy: d18be5073edb4ee733c39d9d823dfe38abdad301

---
+++ _nuxt/static/1650012913/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650012913",layout:"default",error:null,state:{todos:{list:[]},i18n:{routeParams:{}}},serverRendered:a,routePath:"\u002F",config:{},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
